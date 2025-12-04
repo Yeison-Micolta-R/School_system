70d731567b861c67056d4e7d4230ad1b6c518368
Yesenia Castro <yesecastro08@gmail.com>
2025-12-03 20:59:33 -0500

    Arquictetura del appi Backend

---
+++ School_System/src/test/java/com/School_System/app/SchoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.School_System.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
